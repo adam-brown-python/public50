eb4cf34a1014bb34d660a8d4b2709828f8fee12c
adam-brown-python <lazyamazybeats@gmail.com>
2025-09-14 14:09:06 +0330

    Update game.py

--- game/game.py
+++ game/game.py
@@ -4,16 +4,18 @@ while True:
         n = int(input("level: "))
         if n > 0:
             number = random.randint(1,n)
+            break
     except ValueError:
         continue
-    else:
-        break
+
 while True:
     try:
         user_input = int(input("Guess: "))
     except ValueError:
         continue
-    if user_input < number:
+    if user_input <= 0:
+        continue
+    elif user_input < number:
         print("Too small!")
         continue
     elif user_input > number:
